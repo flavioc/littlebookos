50df7b413b62c4a185d9528efa2af206d6e5bec4
Flavio Cruz <flaviocruz@gmail.com>
2021-08-15 17:14:14 -0700

    Add initial files.

---
+++ loader.s
@@ -0,0 +1,28 @@
+global loader
+extern sum_of_three
+
+MAGIC_NUMBER equ 0x1BADB002
+FLAGS equ 0x0
+CHECKSUM equ -MAGIC_NUMBER
+KERNEL_STACK_SIZE equ 4096
+
+section .text:
+align 4
+	dd MAGIC_NUMBER
+	dd FLAGS
+	dd CHECKSUM
+
+loader:
+	mov eax, 0xCAFEBABE
+	mov esp, kernel_stack + KERNEL_STACK_SIZE
+	push dword 3
+	push dword 2
+	push dword 1
+	call sum_of_three
+.loop:
+	jmp .loop
+
+section .bss:
+align 4
+kernel_stack:
+	resb KERNEL_STACK_SIZE
